f487b42b4c57648efadcfeca6a0504ae92e7a221
Mohamed Eid <145832390+MohamedEid20@users.noreply.github.com>
2024-11-13 10:49:20 +0200

    add Register

---
+++ src/pages/Regester/Register.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import "./Register.css";
+
+export default function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [response, setResponse] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password.length < 8) {
+      setMessage("Password must be at least 8 characters.");
+      return;
+    } else if (password !== confirmPassword) {
+      setMessage("Passwords do not match.");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("password", password);
+    formData.append("confirm_password", confirmPassword); 
+
+    try {
+      const res = await fetch(
+        "http://localhost/contact-form-server/register.php",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+
+      const result = await res.json();
+      setResponse(result.message);
+      if (result.redirect) {
+        window.location.href = result.redirect;
+      }
+      setMessage("");
+    } catch (error) {
+      setResponse("Error: " + error.message);
+    }
+  };
+
+  return (
+    <div className="Regisert">
+      <form className="Re_form" onSubmit={handleSubmit}>
+        <h1>User Registration</h1>
+        {message && <p style={{ color: "red" }}>{message}</p>}
+        {response && <p style={{ color: "green" }}>{response}</p>}
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Username"
+          required
+        />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
+        <input
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          placeholder="Confirm Password"
+          required
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
